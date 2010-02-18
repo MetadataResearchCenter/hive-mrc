2b8e1694862c45e9bdb47bff4c95039da50b6adf
jose.aguera <jose.aguera@9d91b754-06b0-11df-8861-21795b59afe9>
2010-02-18 19:27:16 +0000

    sparq engine implementation (not finished)

---
+++ hive-core/src/edu/unc/ils/mrc/hive/api/impl/elmo/SPARQLEngineImpl.java
@@ -0,0 +1,13 @@
+package edu.unc.ils.mrc.hive.api.impl.elmo;
+
+public class SPARQLEngineImpl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
