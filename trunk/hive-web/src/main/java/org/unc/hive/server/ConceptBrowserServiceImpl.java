009622edc323aeeb11862bed409234a31fe0b45a
craig.willis@unc.edu <craig.willis@unc.edu@9d91b754-06b0-11df-8861-21795b59afe9>
2011-04-14 01:18:47 +0000

    Mavenized hive-core project

--- trunk/hive-web/src/main/java/org/unc/hive/server/ConceptBrowserServiceImpl.java
+++ trunk/hive-web/src/main/java/org/unc/hive/server/ConceptBrowserServiceImpl.java
@@ -21,7 +21,7 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 /**
  * The server side implementation of the RPC service.
  */
-@SuppressWarnings("serial")
+
 public class ConceptBrowserServiceImpl extends RemoteServiceServlet implements
 		ConceptBrowserService {
 
