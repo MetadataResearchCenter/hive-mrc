409d3453822e4a442a0c0c20b2c53e50ee35363c
craig.willis@unc.edu <craig.willis@unc.edu@9d91b754-06b0-11df-8861-21795b59afe9>
2011-06-09 14:50:28 +0000

    Modified HIVE 1.0 API to use the new underlying HIVE 2.0 API

--- trunk/hive-core/test/edu/unc/ils/mrc/hive/api/SearcherTest.java
+++ trunk/hive-core/test/edu/unc/ils/mrc/hive/api/SearcherTest.java
@@ -30,16 +30,99 @@ public class SearcherTest
 		String confPath = "/Users/cwillis/dev/hive/conf/hive.properties"; // args[0];
 
 		// Vocabulary name
-		String vocabulary = "nbii"; //args[2];
+		String vocabulary = "lcsh"; //args[2];
 
 		List<String> vocabularies = new ArrayList<String>();
 		vocabularies.add(vocabulary);
 		
 		SKOSServer server = new SKOSServerImpl(confPath);
 		SKOSSearcher searcher = server.getSKOSSearcher();
+		
+		
+	    //searcher.searchAlpha("a");
+	    //searcher.searchAlpha("b");
+	    //searcher.searchAlpha("c");
+		
+		
+		//List<SKOSConcept> concepts = searcher.searchConceptByKeyword("mammals");
+		//for(SKOSConcept concept: concepts) {
+		//	System.out.println(concept.getPrefLabel());
+		//}
+		
+		// 	PREFIX skos: <http://www.w3.org/2004/02/skos/core#> SELECT ?uri ?label
+		//  WHERE { <http://thesaurus.nbii.gov/nbii#Mud> skos:broader ?uri . ?uri skos:prefLabel ?label}"
+		List<HashMap>results = searcher.SPARQLSelect(
+				  "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " + 
+				  "SELECT ?uri ?label WHERE { <http://id.loc.gov/authorities/sh85116169#concept> " + 
+				  "skos:broader ?uri . ?uri skos:prefLabel ?label}",
+				  "lcsh");
+		for (HashMap map: results) {
+			Set<String> keys = map.keySet();
+			for(String key: keys) {
+				Value val = (Value)map.get(key);
+				System.out.println(key + ":" + val);
+			}
+		}
+		
+		
+		results = searcher.SPARQLSelect(
+				  "SELECT ?s ?p ?o WHERE {<http://id.loc.gov/authorities/sh85116169#concept> ?p ?o} LIMIT 10", 
+				  "lcsh");
+		for (HashMap map: results) {
+			Set<String> keys = map.keySet();
+			for(String key: keys) {
+				Value val = (Value)map.get(key);
+				System.out.println(key + ":" + val);
+			}
+		}
+		
+		results = searcher.SPARQLSelect(
+				  //"SELECT ?s ?p ?o WHERE {?s ?p <http://id.loc.gov/authorities/sh85116169#concept>} LIMIT 10", 
+				  //"lcsh");
+						  "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " + 
+						  "DELETE { <http://id.loc.gov/authorities/sh85116169#concept> ?p ?o } ",
+						  "lcsh");
+		for (HashMap map: results) {
+			Set<String> keys = map.keySet();
+			for(String key: keys) {
+				Value val = (Value)map.get(key);
+				System.out.println(key + ":" + val);
+			}
+		}
+		
+		results = searcher.SPARQLSelect(
+				  "SELECT ?s ?p ?o WHERE {<http://id.loc.gov/authorities/sh85116169#concept> ?p ?o} LIMIT 10", 
+				  "lcsh");
+		for (HashMap map: results) {
+			Set<String> keys = map.keySet();
+			for(String key: keys) {
+				Value val = (Value)map.get(key);
+				System.out.println(key + ":" + val);
+			}
+		}
+		
+		/*
+		results = searcher.SPARQLSelect(
+				  "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " + 
+				  "SELECT ?s ?p ?o WHERE { " + 
+				  	"{?s skos:narrower  <http://id.loc.gov/authorities/sh85116169#concept>} " +
+				  	" UNION {?s skos:broader <http://id.loc.gov/authorities/sh85116169#concept>} " +
+				  	" UNION {?s skos:related  <http://id.loc.gov/authorities/sh85116169#concept>} " +
+				  "}",
+				  "lcsh");
+		for (HashMap map: results) {
+			Set<String> keys = map.keySet();
+			for(String key: keys) {
+				Value val = (Value)map.get(key);
+				System.out.println(key + ":" + val);
+			}
+		}
+		*/
+		
+		/*
 		List<HashMap> results = searcher.SPARQLSelect(
 				  "SELECT ?s ?p ?p WHERE {?s ?p ?o} LIMIT 10", 
-				  "nbii");
+				  "lcsh");
 		for (HashMap map: results) {
 			Set<String> keys = map.keySet();
 			for(String key: keys) {
@@ -60,6 +143,7 @@ public class SearcherTest
 				System.out.println(key + ":" + val);
 			}
 		}
+		
 		// 	PREFIX skos: <http://www.w3.org/2004/02/skos/core#> SELECT ?uri ?label WHERE { <http://thesaurus.nbii.gov/nbii#Mud> skos:broader ?uri . ?uri skos:prefLabel ?label}",
 		results = searcher.SPARQLSelect(
 				  "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> " + 
@@ -73,5 +157,6 @@ public class SearcherTest
 				System.out.println(key + ":" + val);
 			}
 		}
+		*/
 	}
 }
