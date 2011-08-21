a5166731c501d7a92b5c3f339e09b248e8efcba2
craig.willis@unc.edu <craig.willis@unc.edu@9d91b754-06b0-11df-8861-21795b59afe9>
2011-08-21 16:15:07 +0000

    Added alt labels to ConceptNode object for for term suggestion widget.

--- trunk/hive-core/src/edu/unc/ils/mrc/hive/api/ConceptTreeBuilder.java
+++ trunk/hive-core/src/edu/unc/ils/mrc/hive/api/ConceptTreeBuilder.java
@@ -3,14 +3,18 @@ package edu.unc.ils.mrc.hive.api;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import javax.xml.namespace.QName;
 
+import org.openrdf.concepts.skos.core.Concept;
+import org.openrdf.elmo.sesame.SesameManager;
+
 public class ConceptTreeBuilder 
 {
 	Map<String, List<String>> conceptTree = new TreeMap<String,List<String>>();
-	Map<String, String> concepts = new TreeMap<String, String>();
+	Map<String, ConceptNode> concepts = new TreeMap<String, ConceptNode>();
 	List<String> topConcepts = new ArrayList<String>();
 	
 	public List<ConceptNode> getTree()
@@ -18,10 +22,11 @@ public class ConceptTreeBuilder
 		List<ConceptNode> cs = new ArrayList<ConceptNode>();
 		for (String uri: topConcepts) 
 		{
-			ConceptNode node = new ConceptNode();
-			String label = concepts.get(uri);
-			node.setLabel(label);
-			node.setUri(uri);
+			ConceptNode node = concepts.get(uri);
+			//ConceptNode node = new ConceptNode();
+			//String label = concepts.get(uri);
+			//node.setLabel(label);
+			//node.setUri(uri);
 			getTree(node, uri);
 			cs.add(node);
 		}
@@ -34,10 +39,11 @@ public class ConceptTreeBuilder
 		if (childURIs != null)
 		{
 			for (String childURI: childURIs) {
-				String label = concepts.get(childURI);
-				ConceptNode child = new ConceptNode();
-				child.setLabel(label);
-				child.setUri(childURI);
+				ConceptNode child = concepts.get(childURI);
+				//String label = concepts.get(childURI);
+				//ConceptNode child = new ConceptNode();
+				//child.setLabel(label);
+				//child.setUri(childURI);
 				node.addChild(child);
 				getTree(child, childURI);
 			}
@@ -52,8 +58,8 @@ public class ConceptTreeBuilder
 		ConceptNode node = new ConceptNode();
 		node.setLabel(prefLabel);
 		node.setUri(uri);
-		
-		concepts.put(uri, prefLabel);
+		node.setAltLabels(concept.getAltLabels());
+		concepts.put(uri, node);
 		
 		TreeMap<String, QName> broaders = concept.getBroaders();
 		if (broaders.size() > 0)
@@ -67,7 +73,12 @@ public class ConceptTreeBuilder
 				
 				String broaderUri = broader.getQName().getNamespaceURI() + broader.getQName().getLocalPart();
 				String broaderLabel = broader.getPrefLabel();
-				concepts.put(broaderUri, broaderLabel);
+				ConceptNode b = new ConceptNode();
+				b.setLabel(broaderLabel);
+				b.setUri(broaderUri);
+				b.setAltLabels(broader.getAltLabels());
+				//concepts.put(broaderUri, broaderLabel);
+				concepts.put(broaderUri, b);
 				
 				List<String> children = conceptTree.get(broaderUri);
 				if (children == null) {
@@ -78,6 +89,52 @@ public class ConceptTreeBuilder
 				conceptTree.put(broaderUri, children);
 				
 				add(broader, searcher);
+			}
+		}
+		else {
+			if (!topConcepts.contains(uri))
+				topConcepts.add(uri);
+		}
+	}
+	
+	
+	public void add(Concept concept, SesameManager manager) 
+	{	
+		String prefLabel = concept.getSkosPrefLabel();
+		String uri = concept.getQName().getNamespaceURI() + concept.getQName().getLocalPart();
+		
+		ConceptNode node = new ConceptNode();
+		node.setLabel(prefLabel);
+		node.setUri(uri);
+		node.setAltLabels(new ArrayList<String>(concept.getSkosAltLabels()));
+		
+		concepts.put(uri, node);
+		
+		Set<Concept> broaders = concept.getSkosBroaders();
+		if (broaders.size() > 0)
+		{
+			for (Concept b: broaders) 
+			{
+				Concept broader = manager.find(Concept.class, b.getQName());
+				
+				String broaderUri = broader.getQName().getNamespaceURI() + broader.getQName().getLocalPart();
+				String broaderLabel = broader.getSkosPrefLabel();
+				ConceptNode cn = new ConceptNode();
+				cn.setLabel(broaderLabel);
+				cn.setUri(broaderUri);
+				cn.setAltLabels(new ArrayList<String>(broader.getSkosAltLabels()));
+				//concepts.put(broaderUri, broaderLabel);
+				concepts.put(broaderUri, cn);
+				
+				List<String> children = conceptTree.get(broaderUri);
+				if (children == null) {
+					children = new ArrayList<String>();
+				}
+				if (!children.contains(uri))
+					children.add(uri);
+				conceptTree.put(broaderUri, children);
+				
+				add(broader, manager);
 			}
 		}
 		else {
