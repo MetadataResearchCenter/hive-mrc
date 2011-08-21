02ad8a3437a4a35345287ac4ff8fe43eb856d3a4
craig.willis <craig.willis@unc.edu>
2011-08-21 17:12:40 +0000

    Updated TermSuggestionServlet to support both tree and list responses.

--- hive-web/src/org/unc/hive/servlet/TermSuggestionServlet.java
+++ hive-web/src/org/unc/hive/servlet/TermSuggestionServlet.java
@@ -2,9 +2,11 @@ package org.unc.hive.servlet;
 
 import java.io.IOException;
 
+
 import java.io.PrintWriter;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -13,66 +15,149 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.unc.hive.client.ConceptProxy;
 import org.unc.hive.server.VocabularyService;
 
 import edu.unc.ils.mrc.hive.api.ConceptNode;
-import edu.unc.ils.mrc.hive.api.SKOSConcept;
-import edu.unc.ils.mrc.hive.ir.lucene.search.AutocompleteTerm;
 
 /**
  * 
  */
-public class TermSuggestionServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	
+public class TermSuggestionServlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 2357815517668304804L;
 
+	static final String PARAM_VOCAB = "cv";
+	static final String PARAM_TEXT = "tx";
+	static final String PARAM_MIN_PHRASE_OCCURRENCES = "mp";
+	static final String PARAM_EXISTING_TERMS = "ex";
+	static final String PARAM_FORMAT = "fmt";
+	
+	static final String FORMAT_TREE = "tree";
+	static final String FORMAT_LIST = "list";
+	
 	public TermSuggestionServlet() {
 		super();
-
 	}
+	
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException 
+	{
 
 		ServletContext context = this.getServletContext();
 		String path = context.getRealPath("");
 		VocabularyService service = VocabularyService.getInstance(path + "/WEB-INF/conf/hive.properties");
 		
-		String vocab = request.getParameter("cv");
-		String text = request.getParameter("text");
-		String mp = request.getParameter("mp");
+		List<String> errors = new ArrayList<String>();
 		
+		// Get the selected vocabularies
+		String vocab = request.getParameter(PARAM_VOCAB);
+		List<String> vocabs = new ArrayList<String>();
+		if (!StringUtils.isEmpty(vocab)) {
+			vocabs = Arrays.asList(vocab.split("\\,"));
+		} else {
+			errors.add("No vocabulary specified");
+		}
+		
+		// Read the input text
+		String text = request.getParameter(PARAM_TEXT);
+		if (StringUtils.isEmpty(text)) {
+			errors.add("No input text specified");
+		}
+		
+		// Get the minimum phrase occurrences, if specified. Otherwise default to 1.
+		String mp = request.getParameter(PARAM_MIN_PHRASE_OCCURRENCES);
 		int minPhraseOccur = 1;
-		try {
-			minPhraseOccur = Integer.valueOf(mp);
-		} catch (NumberFormatException e) {
+		if (StringUtils.isEmpty(mp)) {
+			try {
+				minPhraseOccur = Integer.valueOf(mp);
+			} catch (NumberFormatException e) {}
 		}
 		
-		List<ConceptNode> tree =null;
-		try {
-			List<String> vocabs = new ArrayList<String>();
-			vocabs.add(vocab);
-			if (text == null || text.length() == 0)
-				text = "the quick brown fox jumps over the lazy dog";
-			tree = service.getTagsAsTree(text, vocabs, 15, minPhraseOccur);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// Parse the list of existing terms, if any
+		String existing = request.getParameter(PARAM_EXISTING_TERMS);
+		List<String> existingTermsList = new ArrayList<String>();
+		if (!StringUtils.isEmpty(existing))
+			existingTermsList = Arrays.asList(existing.split("\\|"));
 		
-		String json = "[";
-		for (ConceptNode node: tree) 
+		// Get the requested format
+		String format = request.getParameter(PARAM_FORMAT);
+		if (StringUtils.isEmpty(format))
+			format = FORMAT_TREE;
+		
+		String json = "";
+		try
 		{
-			json += getJson(node) + ",";
+			json = "[";
+			
+			if (format.equals(FORMAT_TREE))
+			{
+				List<ConceptNode> tree =null;
+	
+				tree = service.getTagsAsTree(text, vocabs, 15, minPhraseOccur);
+				if (tree.size() > 0)
+				{
+					int i =0;
+					for (ConceptNode node: tree)  {
+						if (i > 0)
+							json += ", ";
+						json += getJsonTreeItem(node, existingTermsList);
+						i++;
+					}
+				}
+				else
+				{
+					json += "{" +
+						"\"title\": \"No suggestions\"," +
+						"\"hideCheckbox\": true" +
+					"}";
+		
+				}
+			}
+			else
+			{
+				List<ConceptProxy> list =null;
+				list = service.getTags(text, vocabs, 15, minPhraseOccur);
+				if (list.size() > 0)
+				{
+					int i =0;
+					for (ConceptProxy concept: list) {
+						if (i > 0)
+							json += ", ";
+						json += getJsonListItem(concept, existingTermsList);
+						i++;
+					}
+				}
+				else
+				{
+					json += "{" +
+								"\"title\": \"No suggestions\"," +
+								"\"hideCheckbox\": true" +
+							"}";
+				}
+			}
+			json += "]";
+		} catch (Exception e) {
+			errors.add("A server error has occurred.");
 		}
-		json += "]";
-		System.out.println(json);
+			
 		response.setContentType("text/json");
 		PrintWriter writer = new PrintWriter(response.getOutputStream());
-		writer.write(json.toString());
+		if  (errors.size() > 0) {
+			String errorMsg = "";
+			for (String error: errors) 
+				errorMsg += error;
+			errorMsg = getJsonError(errorMsg);
+			writer.write(errorMsg);
+		} else {
+			writer.write(json.toString());
+		}
+		
 		writer.close();
 	}
 
@@ -80,10 +165,17 @@ public class TermSuggestionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 	
-	protected String getJson(ConceptNode node) {
+	protected String getJsonError(String message) {
+		return "[{" +
+			"\"title\": \"" + message + "\"," +
+			"\"hideCheckbox\": true" +
+		"}]";
+	}
+	
+	protected String getJsonTreeItem(ConceptNode node, List<String> ignore) {
 		StringBuffer json = new StringBuffer("{");
 		json.append("\"title\": \"");
 		json.append(node.getLabel());
@@ -92,12 +184,34 @@ public class TermSuggestionServlet extends HttpServlet {
 		json.append("\", \"url\": \"");
 		json.append(node.getUri());
 		json.append("\"");
+		
+		String tooltip = "";
+		List<String> altLabels = node.getAltLabels();
+		if (altLabels != null && altLabels.size() > 0)
+		{
+			tooltip = "Other terms: ";
+			int i=0;
+			for (String altLabel: altLabels) {
+				tooltip += altLabel;
+				if (i < altLabels.size()-1)
+					tooltip += ", ";
+				i++;
+			}
+
+		}
+		json.append(", \"tooltip\": \"");
+		json.append(tooltip);
+		json.append("\"");
+		if (ignore.contains(node.getLabel())) {
+			json.append(",\"select\": true");
+			json.append(",\"unselectable\": \"true\"");
+		}
 		if (node.getChildren().size() > 0) {
 			json.append(", \"isFolder\": \"true\", \"expand\": \"true\", ");
 			json.append("\"children\": [");
 			int i = 1;
 			for (ConceptNode child: node.getChildren()) {
-				String j = getJson(child);
+				String j = getJsonTreeItem(child, ignore);
 				json.append(j);
 				if (i < node.getChildren().size()) {
 					json.append(",");
@@ -108,33 +222,73 @@ public class TermSuggestionServlet extends HttpServlet {
 		}
 		json.append("}");
 		
-
 		return json.toString();
+	}
+	
+	protected String getJsonListItem(ConceptProxy node, List<String> ignore) {
+		StringBuffer json = new StringBuffer("{");
+		json.append("\"title\": \"");
+		json.append(node.getPreLabel());
+		json.append("\", \"key\": \"");
+		json.append(node.getURI());
+		json.append("\", \"url\": \"");
+		json.append(node.getURI());
+		json.append("\", \"origin\": \"");
+		json.append(node.getOrigin());
+		json.append("\", \"score\": \"");
+		json.append(node.getScore());
+		json.append("\"");
+		if (ignore.contains(node.getPreLabel())) {
+			json.append(",\"select\": true");
+			json.append(",\"unselectable\": true");
+		}
+		String tooltip = "";
+		List<String> altLabels = node.getAltLabel();
+		if (altLabels != null && altLabels.size() > 0)
+		{
+			tooltip = "Other terms: ";
+			int i=0;
+			for (String altLabel: altLabels) {
+				tooltip += altLabel;
+				if (i < altLabels.size()-1)
+					tooltip += ", ";
+				i++;
+			}
+
+		}
+		json.append(", \"tooltip\": \"");
+		json.append(tooltip);
+		json.append("\"");
+		HashMap<String, String> broaders = node.getBroader();
+		if (broaders != null && broaders.size() > 0)
+		{
+			json.append(",\"broaders\": [");
+			int i = 1;
+			for (String key: broaders.keySet()) {
+				String uri = broaders.get(key);
+				json.append("{\"title\": \"");
+				json.append(key);
+				json.append("\", \"url\": \"");
+				json.append(uri);
+				json.append("\"}");
+				if (i < broaders.size()) {
+					json.append(",");
+				}
+				i++;
+			}
+			json.append("]");
+			String path = "";
+			for (String broader : broaders.keySet())
+				path += broader;
+			
+			json.append(", \"path\": \"");
+			json.append(path);
+			json.append("\"");
+		}
 		
-		/*
-		[
-		    {title: "item1 with key and tooltip", tooltip: "Look, a tool tip!" },
-		    {title: "item2: selected on init", select: true },
-		    {title: "Folder", isFolder: true, key: "id3",
-		      children: [
-		        {title: "Sub-item 3.1",
-		          children: [
-		            {title: "Sub-item 3.1.1", key: "id3.1.1" },
-		            {title: "Sub-item 3.1.2", key: "id3.1.2" }
-		          ]
-		        },
-		        {title: "Sub-item 3.2",
-		          children: [
-		            {title: "Sub-item 3.2.1", key: "id3.2.1" },
-		            {title: "Sub-item 3.2.2", key: "id3.2.2" }
-		          ]
-		        }
-		      ]
-		    }
-		  ];
-
-				 */
-
+		json.append("}");
+		
+		return json.toString();
 	}
 
 }
